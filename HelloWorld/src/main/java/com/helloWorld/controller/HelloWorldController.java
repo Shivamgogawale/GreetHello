3f10405ef36a510da61d7398c814eaf1b428a90d
Shivam <116865727+Shivamgogawale@users.noreply.github.com>
2023-10-23 15:16:01 +0530

    Update HelloWorldController.java
    Optomise Code Changes

--- HelloWorld/src/main/java/com/helloWorld/controller/HelloWorldController.java
+++ HelloWorld/src/main/java/com/helloWorld/controller/HelloWorldController.java
@@ -9,16 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/greetapi/v1" ,method = {RequestMethod.GET,RequestMethod.POST})
 public class HelloWorldController {
-
 	@GetMapping(value = "/hello")
-	public String gethello()
+	public String gethello(@RequestParam(name = "name",required = false )String name)
 	{
-		return "hello World !!!";
+		if( name !=null)
+		{	return "hello " + name+" !!!";	}
+		else
+			return "hello World !!!";
 	}
 	
-	@GetMapping(value = "/hellowithname")
-	public String getHelloWithName(@RequestParam String name)
-	{
-		return "hello " + name+ " !!!";
-	}
 }
